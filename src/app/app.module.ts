de046c49ca2bcbd28b3784f0ca0dd3afcc5e27d6
Shimon-Levinshtein <61378877+Shimon-Levinshtein@users.noreply.github.com>
2022-03-22 16:18:38 +0200

    finished the footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomePageComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
