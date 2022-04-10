c5cb132d4f54aedcdb871710733c97a64a103a0c
Shimon-Levinshtein <61378877+Shimon-Levinshtein@users.noreply.github.com>
2022-04-10 18:02:55 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'my-skills', component: MySkillsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     HomePageComponent,
     FooterComponent,
+    MySkillsComponent,
   ],
   imports: [
     BrowserModule,
