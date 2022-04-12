8ff83bcca537669b9c1aee37af7169fec3dd342e
Shimon-Levinshtein <61378877+Shimon-Levinshtein@users.noreply.github.com>
2022-04-12 10:20:02 +0300

    start skaiis data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 
 const routes: Routes = [
-  { path: 'my-skills', component: MySkillsComponent }
+  { path: 'my-skills', component: MySkillsComponent },
+  { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
---
+++ src/app/my-skills/skillsData.ts
@@ -0,0 +1,23 @@
+export const skillsDevelop: Array<any> = [
+    {skill: 'HTML', level: 97},
+    {skill: 'Scss', level: 91},
+    {skill: 'JavaScript', level: 98},
+    {skill: 'TypeScript', level: 40},
+
+    {skill: 'Angular', level: 50},
+    {skill: 'React', level: 98},
+    {skill: 'Redux', level: 100},
+    {skill: 'Next.js', level: 55},
+
+    {skill: 'Node.js', level: 80},
+    {skill: 'React Native', level: 60},
+    
+    {skill: 'Mysql', level: 80},
+    {skill: 'Sequelize', level: 96},
+    {skill: 'MongoDB', level: 40},
+    {skill: 'Mongoose', level: 56},
+    {skill: 'Firebase', level: 60},
+    {skill: 'Docker', level: 30},
+    {skill: 'WordPress', level: 35},
+    {skill: 'Gitflow', level: 85},
+];
